e7dbe545af13875958a9ae85c18d33bd678691ac
Anu Mishra <anu2002mishra@gmail.com>
2023-06-05 12:41:54 +0530

    Initial commit

---
+++ demo/src/main/java/com/example/securingApp/SBauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.securingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBauthApplication.class, args);
+	}
+
+}
